31f31dd9b84abd8b9d67ad81c5ea627bdd72eeaf
Nasir ibrahim abba <ibrahimnasir618@gmail.com>
2019-12-06 09:13:11 -0800

    assigmet practical python with dahir

---
+++ more loop.py
@@ -0,0 +1,8 @@
+
+numbers =input('enter your numbers').split()
+first, second, last = int(numbers[0]), int(numbers[1]), int(numbers[2])
+for i in range(first, second, last):
+    print(i)
+    
+
+    
